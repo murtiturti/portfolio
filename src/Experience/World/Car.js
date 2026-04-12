32b5d0fc5455ac9b52d3a0fc5c79215dad2301d3
Murat Diken <diken.murat01@gmail.com>
2026-04-12 00:29:57 -0400

    Translate car up and remove curve as terrain flattens

--- src/Experience/World/Car.js
+++ src/Experience/World/Car.js
@@ -30,6 +30,7 @@ export default class Car
         this.model = this.resource.scene
         this.model.scale.setScalar(1.0)
         this.model.position.y = -7
+        this.baseY = this.model.position.y
         this.scene.add(this.model)
 
         this.model.traverse((child) => {
@@ -57,17 +58,18 @@ export default class Car
 
     update()
     {
-        this.totalMoveTime = this.experience.world.terrain.distance
+        const terrain = this.experience.world.terrain
+        this.totalMoveTime = terrain.distance
+        const curveFactor = 1 - terrain.flattenAmount
 
         // Calculate derivative at point 0.5 (center of terrain uv)
-        const v = 0.5
-        const rotationSampleDelta = 0.001 
+        const rotationSampleDelta = 0.001
 
         const rotationSample0 = (Math.sin(0.3 * 0.5 + this.totalMoveTime) * Math.sin(0.1 * 0.5 + this.totalMoveTime)) * 3.0
         const rotationSample1 = (Math.sin(0.3 * 0.5 + this.totalMoveTime + rotationSampleDelta) * Math.sin(0.1 * 0.5 + this.totalMoveTime + rotationSampleDelta)) * 3.0
         const theta = Math.atan2(rotationSample1 - rotationSample0, rotationSampleDelta)
-        this.model.rotation.y = theta * 0.1
-        this.model.position.x = 0 - (Math.sin(0.3 * 0.5 + this.totalMoveTime * 1.0) * Math.sin(0.1 * 0.5 + this.totalMoveTime * 1.0)) * 3.0
-    
+        this.model.rotation.y = theta * 0.1 * curveFactor
+        this.model.position.x = 0 - (Math.sin(0.3 * 0.5 + this.totalMoveTime) * Math.sin(0.1 * 0.5 + this.totalMoveTime)) * 3.0 * curveFactor
+        this.model.position.y = this.baseY - terrain.baseRoadElevation * terrain.flattenAmount
     }
 }
