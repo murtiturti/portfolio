a10c8e01f513374f7ea6012935095702292d97a5
Murat Diken <diken.murat01@gmail.com>
2026-04-12 01:10:53 -0400

    Support multi-stop gradient texture

--- src/Experience/Utils/GradientTexture.js
+++ src/Experience/Utils/GradientTexture.js
@@ -2,17 +2,22 @@ import * as THREE from 'three'
 
 export default class GradientTexture
 {
-    constructor(color0, color1)
+    // stops: array of [position (0–1), color string] pairs
+    constructor(stops)
     {
         this.canvas = document.createElement('canvas')
+        this.canvas.width = 2
+        this.canvas.height = 512
         this.context = this.canvas.getContext('2d')
 
-        this.gradient = this.context.createLinearGradient(0, 0, 0, window.innerHeight)
-        this.gradient.addColorStop(0, color0)
-        this.gradient.addColorStop(1, color1)
+        this.gradient = this.context.createLinearGradient(0, 0, 0, 512)
+        for (const [pos, color] of stops)
+        {
+            this.gradient.addColorStop(pos, color)
+        }
 
         this.context.fillStyle = this.gradient
-        this.context.fillRect(0, 0, window.innerWidth, window.innerHeight)
+        this.context.fillRect(0, 0, 2, 512)
 
         this.gradientTexture = new THREE.CanvasTexture(this.canvas)
         this.gradientTexture.colorSpace = THREE.SRGBColorSpace
