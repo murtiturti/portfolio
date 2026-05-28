7f1caeb50abaf0b4200c4f36f9642ad93f2845b1
Murat Diken <diken.murat01@gmail.com>
2026-05-28 18:10:21 -0400

    Promote Environment updateMaterials closure to instance method

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -48,26 +48,14 @@ export default class Environment
 
     setEnvironmentMap()
     {
-        this.environmentMap = {}
-        this.environmentMap.intensity = ENV_MAP_INTENSITY
-        this.environmentMap.texture = this.resources.items.environmentMapTexture
+        this.environmentMap = {
+            intensity: ENV_MAP_INTENSITY,
+            texture:   this.resources.items.environmentMapTexture,
+        }
         this.environmentMap.texture.colorSpace = THREE.SRGBColorSpace
-
         this.scene.environment = this.environmentMap.texture
 
-        this.environmentMap.updateMaterials = () =>
-        {
-            this.scene.traverse((child) => 
-            {
-                if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
-                    child.material.envMap = this.environmentMap.texture
-                    child.material.envMapIntensity = this.environmentMap.intensity
-
-                    child.material.needsUpdate = true
-                }
-            })
-        }
-        this.environmentMap.updateMaterials()
+        this.updateEnvironmentMaterials()
 
         // Debug
         if (this.debug.active)
@@ -78,8 +66,21 @@ export default class Environment
                 .min(0)
                 .max(4)
                 .step(0.001)
-                .onChange(this.environmentMap.updateMaterials)
+                .onChange(() => this.updateEnvironmentMaterials())
         }
+    }
+
+    updateEnvironmentMaterials()
+    {
+        this.scene.traverse((child) =>
+        {
+            if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+            {
+                child.material.envMap = this.environmentMap.texture
+                child.material.envMapIntensity = this.environmentMap.intensity
+                child.material.needsUpdate = true
+            }
+        })
     }
 
     
